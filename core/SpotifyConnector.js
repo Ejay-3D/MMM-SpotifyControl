ef596ea561e065a2e8c04ba4eb9398f0560d350a
Ejay <38557267+ejay-ibm@users.noreply.github.com>
2019-03-11 11:43:06 +0100

    Update SpotifyConnector.js

--- core/SpotifyConnector.js
+++ core/SpotifyConnector.js
@@ -15,28 +15,7 @@ module.exports = class SpotifyConnector {
     this.tokenExpiresAt = moment();
   }
 
-  retrieveCurrentlyPlaying() {
-    if (moment().isBefore(this.tokenExpiresAt)) {
-      return this.getSpotifyData();
-
-    } else {
-      return this.refreshAccessToken()
-        .then((response) => {
-          console.log('Refreshed access token because it has expired. Expired at: %s now is: %s',
-            this.tokenExpiresAt.format('HH:mm:ss'), moment().format('HH:mm:ss'));
-
-          this.credentials.accessToken = response.access_token;
-          this.tokenExpiresAt = moment().add(response.expires_in, 'seconds');
-
-          return this.getSpotifyData();
-        })
-        .catch((err) => {
-          console.error('Error while refreshing:');
-          console.error(err);
-        });
-    }
-  }
-  
+    
   playThis(payload) {
 	let url = payload.url;  
 	let uri = replaceall("/", ":", url.replace("https:\/\/open.spotify.com", "spotify"));
@@ -108,61 +87,6 @@ module.exports = class SpotifyConnector {
     
   }
 
-pauseThis(payload) {
-	let url = payload.url;  
-	let uri = replaceall("/", ":", url.replace("https:\/\/open.spotify.com", "spotify"));
-	console.error("pause payload URL is :", (payload.url));
-	console.error("pause payloas URI is :", (uri));
-	
-    if (moment().isBefore(this.tokenExpiresAt)) {
-	  
-	  let currentDeviceID = this.getDeviceID(payload.deviceName);
-	  return this.PauseSpotify(currentDeviceID, uri);
-	
-    } else {
-      return this.refreshAccessToken()
-        .then((response) => {
-          console.log('Refreshed access token because it has expired. Expired at: %s now is: %s',
-            this.tokenExpiresAt.format('HH:mm:ss'), moment().format('HH:mm:ss'));
-
-          this.credentials.accessToken = response.access_token;
-          this.tokenExpiresAt = moment().add(response.expires_in, 'seconds');
-		  let currentDeviceID = this.getDeviceID(payload.deviceName);
-		  return this.PlaySpotify(currentDeviceID, uri);
-        })
-        .catch((err) => {
-          console.error('Error while refreshing:');
-          console.error(err);
-        });
-    }
-  }
-
-  getSpotifyData() {
-    let options = {
-      url: apiEndpoint,
-      headers: {'Authorization': 'Bearer ' + this.credentials.accessToken},
-      json: true
-    };
-
-    return request.get(options);
-  }
-  
-  
-  PauseSpotify(currentDeviceID, uri) {
-    
-		let options = {
-			//url: apiEndpoint + '/play?device_id=' + currentDeviceID, 
-			url: apiEndpoint + '/pause', 			
-			qs: {device_id: currentDeviceID},
-			headers: {'Authorization': 'Bearer ' + this.credentials.accessToken},
-
-			json: true
-		};
-		return request.put(options);	
-
-    
-  }
-
   
   getDeviceID(DeviceName) {
 	
@@ -175,7 +99,7 @@ pauseThis(payload) {
 		};
 		
 		request.put(options).then(function (response) {
-			return response.some(item => item.name === DeviceName).id;
+			return response.some(item => item.name === Name).id;
 		}).catch(function (err) {
 			return err;
 		})
