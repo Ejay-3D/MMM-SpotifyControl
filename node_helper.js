003580bb70e19c939c81fc168d3c0d228b30c9ef
Ejay <38557267+ejay-ibm@users.noreply.github.com>
2019-03-11 15:43:36 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -14,11 +14,11 @@ module.exports = NodeHelper.create({
     switch (notification) {
       case 'CONNECT_TO_SPOTIFY':
         this.connector = new SpotifyConnector(payload);
-        this.retrieveCurrentSong();
+       
         break;
 
       case 'UPDATE_CURRENT_SONG':
-        this.retrieveCurrentSong();
+       
         break;
 		    
       case 'PLAY_SPOTIFY':
@@ -58,55 +58,4 @@ module.exports = NodeHelper.create({
     }
   },
 
-
-  retrieveCurrentSong: function () {
-    this.connector.retrieveCurrentlyPlaying()
-      .then((response) => {
-        if (response) {
-          this.sendRetrievedNotification(response);
-        } else {
-          this.sendRetrievedNotification({ noSong: true });
-        }
-      })
-      .catch((error) => {
-        console.error('Can’t retrieve current song. Reason: ');
-        console.error(error);
-      });
-  },
-
-
-  sendRetrievedNotification: function (songInfo) {
-    let payload = songInfo;
-
-    if (!songInfo.noSong) {
-      payload = {
-        imgURL: this.getImgURL(songInfo.item.album.images),
-        songTitle: songInfo.item.name,
-        artist: this.getArtistName(songInfo.item.artists),
-        album: songInfo.item.album.name,
-        titleLength: songInfo.item.duration_ms,
-        progress: songInfo.progress_ms,
-        isPlaying: songInfo.isPlaying,
-        deviceName: songInfo.device.name
-      };
-    }
-
-    this.sendSocketNotification('RETRIEVED_SONG_DATA', payload);
-  },
-
-
-  getArtistName: function (artists) {
-    return artists.map((artist) => {
-      return artist.name;
-    }).join(', ');
-  },
-
-
-  getImgURL(images) {
-    let filtered = images.filter((image) => {
-      return image.width >= 240 && image.width <= 350;
-    });
-
-    return filtered[0].url;
-  }
 });
