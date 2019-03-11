8e6208e3d708fc7a2a235bb5e99ee12c4318adb8
Ejay <38557267+ejay-ibm@users.noreply.github.com>
2019-03-11 11:47:47 +0100

    Update MMM-NowPlayingOnSpotify.js

--- MMM-NowPlayingOnSpotify.js
+++ MMM-NowPlayingOnSpotify.js
@@ -1,17 +1,17 @@
 'use strict';
 
-Module.register('MMM-NowPlayingOnSpotify', {
+Module.register('MMM-SpotifyControl', {
 
   // default values
   defaults: {
     // Module misc
-    name: 'MMM-NowPlayingOnSpotify',
-    hidden: false,
+    name: 'MMM-SpotifyControl',
+    hidden: true,
 
     // user definable
     updatesEvery: 1,          // How often should the table be updated in s?
-    showCoverArt: true,       // Do you want the cover art to be displayed?
-	deviceName: null,		  // Set to Spotify device name to controll specific device.
+  //  showCoverArt: true,       // Do you want the cover art to be displayed?
+	  deviceName: null,		  // Set to Spotify device name to controll specific device.
   },
 
 
@@ -34,20 +34,21 @@ Module.register('MMM-NowPlayingOnSpotify', {
     }
   },
 
-  getStyles: function () {
-    return [
-      this.file('css/styles.css'),
-      this.file('node_modules/moment-duration-format/lib/moment-duration-format.js'),
-      'font-awesome.css'
-    ];
-  },
+	//commenting here as I'm not sure it's necessary for controller 
+ // getStyles: function () {
+//    return [
+ //     this.file('css/styles.css'),
+ //     this.file('node_modules/moment-duration-format/lib/moment-duration-format.js'),
+ //     'font-awesome.css'
+ //   ];
+ // },
 
-  getScripts: function () {
-    return [
-      this.file('core/NPOS_DomBuilder.js'),
-      'moment.js'
-    ];
-  },
+//  getScripts: function () {
+ //   return [
+//      this.file('core/NPOS_DomBuilder.js'),
+ //     'moment.js'
+  //  ];
+ // },
   
   
   notificationReceived: function(notification, payload, sender) {
@@ -60,12 +61,10 @@ Module.register('MMM-NowPlayingOnSpotify', {
         break;
 		
       case 'PLAY_NEXT':
-        //tentative by ejay
 payload["deviceName"] = this.config.deviceName;
                 this.sendSocketNotification('PLAY_NEXT', payload);	
        break;
       case 'PLAY_PREVIOUS':
-        //tentative by ejay
 payload["deviceName"] = this.config.deviceName;
                 this.sendSocketNotification('PLAY_PREVIOUS', payload);
         break;
@@ -90,31 +89,5 @@ payload["deviceName"] = this.config.deviceName;
   
   },
 
-
-  socketNotificationReceived: function (notification, payload) {
-    switch (notification) {
-      case 'RETRIEVED_SONG_DATA':
-        this.initialized = true;
-        this.context = payload;
-        this.updateDom();
-    }
-  },
-
-  startFetchingLoop() {
-    // start immediately ...
-    let credentials = {
-      clientID: this.config.clientID,
-      clientSecret: this.config.clientSecret,
-      accessToken: this.config.accessToken,
-      refreshToken: this.config.refreshToken
-    };
-
-    this.sendSocketNotification('CONNECT_TO_SPOTIFY', credentials);
-
-    // ... and then repeat in the given interval
-    setInterval(() => {
-      this.sendSocketNotification('UPDATE_CURRENT_SONG');
-    }, this.config.updatesEvery * 1000);
-  }
 });
 
