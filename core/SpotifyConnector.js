9ab6cda799977ad28a0d4185cbc67845f72dfedd
Ejay <38557267+ejay-ibm@users.noreply.github.com>
2019-03-12 13:01:08 +0100

    Update SpotifyConnector.js

--- core/SpotifyConnector.js
+++ core/SpotifyConnector.js
@@ -245,17 +245,18 @@ PreviousSpotify(currentDeviceID) {
     resolveWithFullResponse: true
   }
   var foundId = null
+  var devices = null
    const request = async () => {
    const response = await fetch('https://api.spotify.com/v1/me/player/devices',options);
    const result = await response.json(); 
-   var devices = result.body.devices();
+   devices = result.body.devices
 	   console.error('we are here result is :');
 	console.error(result);
 	   console.error('we are here devices is :');
 	console.error(devices);
    
    //You should check devices are null or empty.
-    foundId = devices[0].id();
+    foundId = devices[0].id
     for (i = 0; i < devices.length; i++) {
       var device = devices[i]
       if (device.name == deviceName) {
