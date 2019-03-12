4bf1ce0b5447dbe1b7e7537e61964bf044b3822c
Ejay <38557267+ejay-ibm@users.noreply.github.com>
2019-03-12 15:29:35 +0100

    Update SpotifyConnector.js

--- core/SpotifyConnector.js
+++ core/SpotifyConnector.js
@@ -274,16 +274,13 @@ PreviousSpotify(currentDeviceID) {
 	    console.error('device name from config:');
 	console.error(deviceName);
       if (device.name == deviceName) foundId = device.id;
-  
-	    
     }
 	 console.error('we are here my found id for mirror is :');
 	console.error(foundId);
-	    
 	   
 }
-    return foundId;
-    request();
+    
+    request(foundId);
 }
   
 
